96353af3c6868896e8d048c24f8f9e1432283caf
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-08-25 18:27:24 +0200

    create socials

---
+++ config/socials.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    [
+        "picture" => "footer-facebook.png",
+        "name_social" => "facebook",
+    ],
+    [
+        "picture" => 'footer-periscope.png',
+        "name_social" => 'periscope',
+    ],
+    [
+
+        "picture" => 'footer-pinterest.png',
+        "name_social" => 'pinterest',
+    ],
+    [
+
+        "picture" => 'footer-twitter.png',
+        "name_social" => 'twitter',
+    ],
+    [
+
+        "picture" => 'footer-youtube.png',
+        "name_social" => 'youtube',
+    ]
+];
