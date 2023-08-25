458578b404b9465a841cf5c3cee9f364095294f3
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-08-25 18:27:35 +0200

    create voices

---
+++ config/voices.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+
+    [
+        "text" => "Characters",
+        "route" => "character",
+    ],
+    [
+        'text' => 'Comics',
+        'route' => 'comic.list',
+    ],
+    [
+        'text' => 'Movies',
+        'route' => 'movies',
+    ],
+    [
+        'text' => 'TV',
+        'route' => 'tv',
+    ],
+    [
+        'text' => 'Collectibles',
+        'route' => 'collectibles',
+    ],
+    [
+        'text' => 'Videos',
+        'route' => 'videos',
+    ],
+    [
+        'text' => 'Fans',
+        'route' => 'fans',
+    ],
+    [
+        'text' => 'News',
+        'route' => 'news',
+    ],
+    [
+        'text' => 'Shop',
+        'route' => 'shop',
+    ],
+
+];
