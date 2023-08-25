61cbf42ed1d07b682c1785ef56e65f6d214d60bb
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-08-25 20:40:43 +0200

    set show page

--- resources/views/comic/show.blade.php
+++ resources/views/comic/show.blade.php
@@ -15,33 +15,33 @@
 
     <section class="details">
 
-        <div class="detail-container">
+        <div class="detail-container row justify-content-center mx-auto">
 
-            <div class="col">
+            <div class="col-7">
 
                 <div class="info-detail">
 
-                    <h1>{{ $comic->title }}</h1>
+                    <h1 class="mb-3">{{ $comic->title }}</h1>
 
-                    <div id="info-avaible">
+                    <div id="info-avaible" class="row my-3">
 
-                        <div>
+                        <div class="col-8">
                             <h4>U.S. Price:
-                                <span>
+                                <span class="text-white">
                                     {{ $comic->price }}
                                 </span>
                             </h4>
                             <h4>Avaible</h4>
                         </div>
-                        <div>
-                            <h4>Check Avaiblety</h4>
+                        <div class="col-4">
+                            <h4 class="text-white">Check Avaiblety</h4>
                         </div>
                     </div>
 
                     <p>{{ $comic->description }}</p>
                 </div>
             </div>
-            <div class="col">
+            <div class="col-4">
                 <h1>Advertisment</h1>
 
                 <figure>
@@ -51,47 +51,50 @@
         </div>
 
         <div class="detail-product">
-            <div class="detail-product-container">
+            <div class="container">
 
-                <div>
-                    <h2>Talent</h2>
-                    <ul>
-                        <li>
-                            <h3>Art by:</h3>
-                            <h3>
-                                {{ $comic->artists }}
-                            </h3>
-                        </li>
+                <div class="row row-cols-2">
 
-                        <li>
-                            <h3>Written by:</h3>
-                            <h3>
-                                {{ $comic->writers }}
-                            </h3>
-                        </li>
+                    <div>
+                        <h2 class="border-bottom border-secondary-emphasis">Talent</h2>
+                        <ul class="p-0">
+                            <li class="d-flex justify-content-between border-bottom border-secondary-emphasi">
+                                <h3 class="me-3">Art by:</h3>
+                                <h3 class="w-75">
+                                    {{ $comic->artists }}
+                                </h3>
+                            </li>
 
-                    </ul>
-                </div>
+                            <li class="d-flex justify-content-between border-bottom border-secondary-emphasi">
+                                <h3 class="me-3">Written by:</h3>
+                                <h3 class="w-75">
+                                    {{ $comic->writers }}
+                                </h3>
+                            </li>
 
-                <div>
-                    <h2>Specs</h2>
-                    <ul>
-                        <li>
-                            <h3>Series:</h3>
-                            <h3>{{ $comic->series }}</h3>
-                        </li>
+                        </ul>
+                    </div>
 
-                        <li>
-                            <h3>U.S.Price:</h3>
-                            <h3>{{ $comic->price }}</h3>
-                        </li>
+                    <div>
+                        <h2 class="border-bottom border-secondary-emphasi">Specs</h2>
+                        <ul class="p-0">
+                            <li class="d-flex justify-content-between border-bottom border-secondary-emphasi">
+                                <h3 class="me-3">Series:</h3>
+                                <h3>{{ $comic->series }}</h3>
+                            </li>
 
-                        <li>
-                            <h3>On Sale Date:</h3>
-                            <h3>{{ $comic->sale_date }}</h3>
-                        </li>
+                            <li class="d-flex justify-content-between border-bottom border-secondary-emphasi">
+                                <h3 class="me-3">U.S.Price:</h3>
+                                <h3>{{ $comic->price }}</h3>
+                            </li>
 
-                    </ul>
+                            <li class="d-flex justify-content-between border-bottom border-secondary-emphasi">
+                                <h3 class="me-3">On Sale Date:</h3>
+                                <h3>{{ $comic->sale_date }}</h3>
+                            </li>
+
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
