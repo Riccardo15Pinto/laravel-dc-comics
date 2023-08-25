747961f6a912d0497ab5759633a5e79badc52cf0
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-08-25 16:58:31 +0200

    set index template

--- resources/views/comic/index.blade.php
+++ resources/views/comic/index.blade.php
@@ -1,3 +1,16 @@
 @extends('layouts.main')
 
 @section('title', 'Home')
+
+
+@section('main-content')
+    <ul>
+        @foreach ($comics as $comic)
+            <li>
+                <a href="{{ route('show', ['comic' => $comic->id]) }}">
+                    {{ $comic->title }}
+                </a>
+            </li>
+        @endforeach
+    </ul>
+@endsection
