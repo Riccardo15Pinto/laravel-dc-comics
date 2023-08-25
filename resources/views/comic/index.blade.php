6eed1e29febcbd8401fc84f21f65d6b9b64aa84d
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-08-25 20:01:26 +0200

    set index page

--- resources/views/comic/index.blade.php
+++ resources/views/comic/index.blade.php
@@ -2,16 +2,35 @@
 
 @section('title', 'Home')
 
-
+@section('item')
+    <div class="flag">
+        CURRENT SERIES
+    </div>
+@endsection
 @section('main-content')
-    <ul>
-        @foreach ($comics as $comic)
-            <li>
-                <a href="{{ route('comics.show', ['comic' => $comic->id]) }}">
-                    {{ $comic->title }}
-                </a>
-            </li>
-        @endforeach
-    </ul>
-    <a href="{{ route('comics.create') }}">Aggiungi fumetto</a>
+    <section class="home-main">
+
+        <div class="container">
+            <h1 class="text-center py-3 text-white">COMICS</h1>
+            <div class="row row-cols-4">
+                @foreach ($comics as $comic)
+                    <div class="p-3 text-center">
+                        <a href="{{ route('comics.show', ['comic' => $comic->id]) }}">
+                            <figure class="d-block mx-auto">
+                                <img src="{{ $comic->thumb }}" alt="">
+                            </figure>
+                            <h2> {{ $comic->title }}</h2>
+                            <h3>{{ $comic->series }}</h3>
+                        </a>
+                    </div>
+                @endforeach
+
+            </div>
+            <div class="text-center py-3">
+                <a href="#" class="btn btn-primary">LOAD MORE</a>
+                <a href="{{ route('comics.create') }}" class="btn btn-success">Aggiungi fumetto</a>
+            </div>
+        </div>
+    </section>
+
 @endsection
