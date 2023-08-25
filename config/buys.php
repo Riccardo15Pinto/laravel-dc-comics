04d59d9a0332ffa6c84418e4fa19ff7bdf5b68cb
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-08-25 18:27:12 +0200

    create buys

---
+++ config/buys.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    [
+
+        "pic" => "buy-comics-digital-comics.png",
+        "name" => "DIGITAL COMICS",
+    ],
+    [
+
+        "pic" => 'buy-comics-merchandise.png',
+        "name" =>  'DC MERCHANDISE',
+    ],
+    [
+
+        "pic" =>  'buy-comics-shop-locator.png',
+        "name" =>  'SUBSCRIPTION',
+    ],
+    [
+        "pic" => 'buy-comics-subscriptions.png',
+        "name" =>  'COMIC SHOP LOCATOR',
+
+    ],
+    [
+
+        "pic" =>  'buy-dc-power-visa.svg',
+        "name" =>  'DC POWER VISA',
+    ]
+];
